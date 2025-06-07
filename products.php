f92cb2112f57d1760f72955fb6b2acd3bf3daac7
Sag21 <sagarsakhare183@gmail.com>
2025-06-08 00:22:43 +0530

    Header style

--- products.php
+++ products.php
@@ -16,14 +16,16 @@ if ($conn->connect_error) {
 </head>
 <body>
     <header>
-        <h1>Padma Oil Industry</h1>
-        <nav>
-            <a href="index.html">Home</a>
-            <a href="products.php">Products</a>
-            <a href="about.html">About Us</a>
-            <a href="cart.html">Cart</a>
-            <a href="contact.html" class="active">Contact</a>
-        </nav>
+        <div class="header-content">
+            <h1>Padma Oil Industry</h1>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="products.php">Products</a>
+                <a href="about.html">About Us</a>
+                <a href="cart.html">Cart</a>
+                <a href="contact.html" class="active">Contact</a>
+            </nav>
+        </div>
     </header>
 
     <main>
